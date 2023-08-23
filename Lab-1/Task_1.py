372f5b041fe6c79fdb5932797e3abfcb4e84f8ee
Shajib Hossain <61625255+younngDumbBrokeShajib@users.noreply.github.com>
2023-08-24 03:54:53 +0600

    Update Task_1.py

--- Lab-1/Task_1.py
+++ Lab-1/Task_1.py
@@ -1,54 +1,14 @@
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
+import random
 
 
 def draw_points(x, y):
-    #glPointSize(5) #pixel size. by default 1 thake
-    glBegin(GL_LINES) #for base and 2 corners
-    glVertex2f(100,0)#base
-    glVertex2f(300, 0)
-    glVertex2f(300, 0)#right corner
-    glVertex2f(300, 200)
-    glVertex2f(300, 200)#roof
-    glVertex2f(100, 200)
-    glVertex2f(100, 200)#left corner
-    glVertex2f(100, 0)
-    glEnd()
-
-
-    glBegin(GL_QUADS) #for windows
-    glVertex2f(280,85)
-    glVertex2f(280, 100)
-    glVertex2f(260, 100)
-    glVertex2f(260, 85)
-    glEnd()
-
-    glBegin(GL_QUADS)#for left window
-    glVertex2f(120, 85)
-    glVertex2f(140, 85)
-    glVertex2f(140, 100)
-    glVertex2f(120, 100)
-    glEnd()
-
-
-    glBegin(GL_QUADS) #doors
-    glVertex2f(150,0)
-    glVertex2f(200, 0)
-    glVertex2f(200, 75)
-    glVertex2f(150, 75)
-    glEnd()
-
-    glBegin(GL_TRIANGLES)# roof er uporer tirangle
-    glVertex2f(100,200)
-    glVertex2f(300, 200)
-    glVertex2f(200, 300)
-    glEnd()
-
+    glPointSize(10) #pixel size. by default 1 thake
     glBegin(GL_POINTS)
-    glVertex2f(100, 0)
+    glVertex2f(x,y) #jekhane show korbe pixel
     glEnd()
-
 
 
 def iterate():
@@ -63,9 +23,14 @@ def showScreen():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
     iterate()
-    glColor3f(100.0, 0.0, 0.0) #konokichur color set (RGB)
+    glColor3f(1.0, 1.0, 0.0) #konokichur color set (RGB)
     #call the draw methods here
-    draw_points(0, 0)
+
+    #generating 50 random pixels
+    for i in range(0,50):
+        x_cord=random.randint(100,500)
+        y_cord=random.randint(100,500)
+        draw_points(x_cord, y_cord)
     glutSwapBuffers()
 
 
